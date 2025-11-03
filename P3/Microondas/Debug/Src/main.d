6c4aafb38bf390aebf130276d437ccce60b0f517
RodrigoQ99 <149353418+RodrigoQ99@users.noreply.github.com>
2025-11-03 10:47:44 -0600

    Código Microondas

---
+++ P3/Microondas/Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/Maria\ Priscila\ Quiro/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.3/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h \
+ C:/Users/Maria\ Priscila\ Quiro/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.3/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l053xx.h \
+ C:/Users/Maria\ Priscila\ Quiro/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.3/Drivers/CMSIS/Include/core_cm0plus.h \
+ C:/Users/Maria\ Priscila\ Quiro/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.3/Drivers/CMSIS/Include/cmsis_version.h \
+ C:/Users/Maria\ Priscila\ Quiro/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.3/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/Maria\ Priscila\ Quiro/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.3/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Maria\ Priscila\ Quiro/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.3/Drivers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/Maria\ Priscila\ Quiro/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.3/Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h
+C:/Users/Maria\ Priscila\ Quiro/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.3/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h:
+C:/Users/Maria\ Priscila\ Quiro/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.3/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l053xx.h:
+C:/Users/Maria\ Priscila\ Quiro/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.3/Drivers/CMSIS/Include/core_cm0plus.h:
+C:/Users/Maria\ Priscila\ Quiro/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.3/Drivers/CMSIS/Include/cmsis_version.h:
+C:/Users/Maria\ Priscila\ Quiro/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.3/Drivers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/Maria\ Priscila\ Quiro/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.3/Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/Maria\ Priscila\ Quiro/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.3/Drivers/CMSIS/Include/mpu_armv7.h:
+C:/Users/Maria\ Priscila\ Quiro/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.3/Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h:
